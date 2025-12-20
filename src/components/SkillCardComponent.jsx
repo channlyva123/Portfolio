e13d03f1bec2667974fcfecaa103bdd459716192
vachannly <vchannly@gmail.com>
2025-12-20 22:46:41 +0700

    fix shadow

--- src/components/SkillCardComponent.jsx
+++ src/components/SkillCardComponent.jsx
@@ -2,7 +2,7 @@ import Popup from "@/animations/Popup";
 const SkillCardComponent = ({ skill }) => {
   return (
     <Popup delay={200}>
-      <div className='max-w-[120px] group shadow-xl border py-2 rounded-md border-text cursor-pointer'>
+      <div className='max-w-[120px] group shadow-xl border py-2 rounded-md border-gray-200 cursor-pointer'>
         <div className='w-[50%] h-[50%] m-auto flex justify-center items-center'>
           <img
             src={skill.src}
@@ -11,7 +11,9 @@ const SkillCardComponent = ({ skill }) => {
           />
         </div>
 
-        <p className='hyphens-auto text-center py-2'>{skill.subject}</p>
+        <p className='hyphens-auto text-center text-gray-400 py-2'>
+          {skill.subject}
+        </p>
       </div>
     </Popup>
   );
